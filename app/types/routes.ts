6eddbde2c2b7882501445f56c1da36bacb6e65be
Keisuke Kido <kskkido@gmail.com>
2019-08-12 13:46:32 +0900

    create route types

---
+++ app/types/routes.ts
@@ -0,0 +1,45 @@
+import {
+  StaticContext,
+  RouteProps as InnerRouteProps,
+  RouteComponentProps as InnerRouteComponentProps,
+} from 'react-router';
+
+export type RouteComponentProps<
+  P = Record<string, any>,
+  C = StaticContext,
+  S = any
+> = (
+  InnerRouteComponentProps<
+    P,
+    C,
+    S
+  > &
+  {
+    routeConfigs?: RouteConfig[];
+  }
+);
+
+export type RouteProps = (
+  InnerRouteProps &
+  RouteFilterPayload &
+  {
+    path: string;
+    component: React.ComponentType<any>;
+    routeConfigs?: RouteConfig[];
+  }
+);
+
+export type RouteConfig = {
+  filter?: RouteFilter;
+  component: React.ComponentType<RouteProps>
+  props: Pick<RouteProps, 'path' | 'component' | 'routeConfigs'>;
+};
+
+export type RouteFilter<S = any> = (
+  (state: S) => RouteFilterPayload
+);
+
+export type RouteFilterPayload = {
+  status: 'valid' | 'invalid' | 'loading';
+  redirectUrl: string;
+};
