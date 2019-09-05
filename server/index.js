ab60c8ac84a6e75f933a4871860ea9e527c8506f
Keisuke Kido <kskkido@gmail.com>
2019-09-05 10:31:36 +0900

    create https connection

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+const https = require('https')
 const express = require('express');
 const consola = require('consola');
 const config = require('./config');
@@ -8,11 +10,16 @@ const app = express();
 app.use(middleware);
 
 if (module === require.main) {
-  const server = app.listen(config.port, () => {
-    const { address } = server.address();
-    const host = address === '::' ? 'localhost' : address;
-    const urlSafeHost = host.includes(':') ? `[${host}]` : host;
-
-    consola.success(`Listening on http://${urlSafeHost}:${config.port}`);
-  });
+  const server = (
+    https.createServer({
+      key: fs.readFileSync('server.key'),
+      cert: fs.readFileSync('server.cert')
+    }, app).listen(config.port, () => {
+      const { address } = server.address();
+      const host = address === '::' ? 'localhost' : address;
+      const urlSafeHost = host.includes(':') ? `[${host}]` : host;
+  
+      consola.success(`Listening on https://${urlSafeHost}:${config.port}`);
+    })
+  )
 }
